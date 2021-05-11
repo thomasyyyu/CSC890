9b3006a0401d4b80db0c4d141a880176c8488da0
Thomas Yu <yyu10@mail.sfsu.edu>
2021-05-10 19:20:50 -0700

    Finished project

---
+++ Index.js
@@ -0,0 +1,10 @@
+fetch('data.json')
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    appendData(data);
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
